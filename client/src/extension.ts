799856e955897fcec5f514982c348dcf7fc037b6
Kaiyan XIAO <k.max.xiao@gmail.com>
2020-07-20 23:10:54 +0100

    Added launch and closure of the mro server by the extension. (#14)

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,9 +3,15 @@ import * as net from 'net';
 import {
     LanguageClient, ServerOptions, TransportKind, LanguageClientOptions
 } from 'vscode-languageclient';
-import { ExtensionContext, window, commands } from "vscode";
+import { ExtensionContext, window, commands, workspace, extensions } from "vscode";
+import * as cp from "child_process";
 
 let client : LanguageClient;
+let mroServerProcess : cp.ChildProcess;
+
+async function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 export function activate(context: ExtensionContext) {
     // get the absolute path of the compiled server.js
@@ -15,10 +21,12 @@ export function activate(context: ExtensionContext) {
     let debugOptions = {
         execArgv: ['--nolazy', '--inspect=6009']
     };
+    let connectionEstablished = false;
     const serverOptions: ServerOptions = function() {
 		return new Promise((resolve, reject) => {
             var socketClient = new net.Socket();
 			socketClient.connect(3000, "127.0.0.1", function() {
+                connectionEstablished = true;
 				resolve({
                     reader: socketClient,
                     writer: socketClient
@@ -48,9 +56,41 @@ export function activate(context: ExtensionContext) {
 
     context.subscriptions.push(commands.registerCommand(showMroCommand, showMroHandler));
 
+    // let root_folder = workspace.workspaceFolders[0].uri.fsPath + '/../../';
+    let extPath = extensions.getExtension('kaiyan.python-mro').extensionPath;
+    let mroServerProcess = cp.exec(`cd ${extPath}; pipenv run python -m python_server.server`, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(stdout);
+    });
+
+    async function terminateMroServer() {
+        // cp.exec('fuser -k 3000/tcp');
+        let signal_result = mroServerProcess.kill('SIGINT');
+        let killed = mroServerProcess.killed;
+        process.exit(0);
+    }
+
+    process.on('SIGINT', terminateMroServer);
+    process.on('exit', terminateMroServer);
+
+    // delay the start of the client until the MRO server is connected
+    (async () => {
+        let startWaitTime = Date.now();
+        while (!connectionEstablished) {
+            await sleep(1000);
+            if (Date.now() - startWaitTime > 3000) {
+                break;
+            }
+        }
+        client.start();
+    })();
+
     // start the client
     // this will also launch the server by launching the complied script
-    client.start();
+    // client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
