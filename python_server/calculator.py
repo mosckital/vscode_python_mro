1b26831e90e98c58c3f9e0bd8e4f2aacc6550c60
Kaiyan XIAO <k.max.xiao@gmail.com>
2020-08-01 23:46:07 +0100

    Added function to simply extract a name from codes and its unit test. (#18)

--- python_server/calculator.py
+++ python_server/calculator.py
@@ -5,33 +5,78 @@ from jedi.api.classes import Name
 
 
 class MROCalculator(type(Name)):
+    """
+	This class gathers the methods to calculate the MRO list given a Jedi Name
+	representing a class name.
+	"""
 
-	@staticmethod
-	def get_parent_class_str_from_class_name(name: Name) -> str:
-		if name.type != 'class':
-			raise ValueError('Should be a jedi Name for class!')
-		start_pos = name.get_definition_start_position()
-		end_pos = name.get_definition_end_position()
-		lines = name.get_line_code(after=end_pos[0] - start_pos[0]).splitlines()
-		lines[-1] = lines[-1][:end_pos[1]]
-		lines[0] = lines[0][start_pos[1]:]
-		sig_str = (''.join(lines)).split(':')[0]
-		assert sig_str.startswith('class ')
-		if '(' not in sig_str:
-			return []  # no parent class, only 'object'
-		return sig_str[sig_str.index('(') + 1 : len(sig_str) - 1]
+    @staticmethod
+    def get_parent_class_str_from_class_name(name: Name) -> str:
+        """
+		To fetch the string of the parent class list in the class declaration
+		signature from a given class Jedi Name.
+
+		Args:
+			name: the given Jedi Name of the target class
+		
+		Returns:
+			the fetched string of the parent class list
+		"""
+        # check if the given name represents a class
+        if name.type != 'class':
+            raise ValueError('Should be a jedi Name for class!')
+        # get the start and end positions and then the lines between (inclusive)
+        start_pos = name.get_definition_start_position()
+        end_pos = name.get_definition_end_position()
+        lines = name.get_line_code(after=end_pos[0] - start_pos[0]).splitlines()
+        # trim the unwanted part in the first and the last lines
+        lines[-1] = lines[-1][:end_pos[1]]
+        lines[0] = lines[0][start_pos[1]:]
+        # join the lines into a single string
+        sig_str = (''.join(lines)).split(':')[0]
+        # the joined string should start with word `class`
+        assert sig_str.startswith('class ')
+        # case that no parent class
+        if '(' not in sig_str:
+            return ""  # no parent class, only 'object'
+        # case that having parent classes surrounded by a pair of parentheses
+        return sig_str[sig_str.index('(') + 1 : len(sig_str) - 1]
+
+    @staticmethod
+    def get_name_at_pos(
+			lines: Sequence[str], row: int, col: int
+		) -> Tuple[str, int, int]:
+        """
+		Get the entity name at the given position.
+
+		Args:
+			lines: the lines of codes to analyse
+			row: the row of the target entity in the given lines
+			col: the column of the target entity in the given lines
+		
+		Returns:
+			(the name, the row of the name's end, the column of the name's end)
+		"""
+        idx = col
+		# a name should start with alphabet and continue with alphabet, number
+		# or underscore
+		# a name should be in the same line, not separated by any other char
+        if lines[row][idx].isalpha():
+            while lines[row][idx].isalnum() or lines[row][idx] == '_':
+                idx += 1
+        return lines[row][col:idx], row, idx
 
 
 class Bracket(IntEnum):
 
-	ROUND = 1
-	SQUARE = 2
-	CURLY = 3
+    ROUND = 1
+    SQUARE = 2
+    CURLY = 3
 
 
 class ParentClass:
 
-	def __init__(self, name: str, pos: Tuple[int, int]) -> None:
-		self.name = name
-		self.pos = pos
-		self.params : Sequence[Tuple[Bracket, Sequence[ParentClass]]] = None
+    def __init__(self, name: str, pos: Tuple[int, int]) -> None:
+        self.name = name
+        self.pos = pos
+        self.params : Sequence[Tuple[Bracket, Sequence[ParentClass]]] = None
--- test_mro_server/test_calculator.py
+++ test_mro_server/test_calculator.py
@@ -2,7 +2,7 @@ import pytest
 import jedi
 from os import path
 from jedi.api.classes import Name
-from typing import Sequence
+from typing import Sequence, Tuple
 from python_server.calculator import MROCalculator
 
 
@@ -21,12 +21,29 @@ DIAMOND_PARENT_CLASS_STRINGS = [
 
 
 class TestMROCalculator:
+	"""Test suite for the MROCalculator class."""
 
 	@pytest.mark.parametrize(
         ('name', 'expected',),
         list(zip(DIAMOND_CLASS_NAMES, DIAMOND_PARENT_CLASS_STRINGS))
     )
 	def test_get_parent_class_str_from_class_name(
-		self, name: Name, expected: str
-	):
+			self, name: Name, expected: str
+		):
+		"""Unit test for get_parent_class_str_from_class_name()."""
 		assert MROCalculator.get_parent_class_str_from_class_name(name) == expected
+
+	@pytest.mark.parametrize(
+		('file_path', 'line', 'char', 'expected'),
+		[
+			(DIAMOND_FILE_PATH, l, c, (n, l, c + 1))
+			for (l, c), n in zip(DIAMOND_CLASS_NAME_LOCATIONS, ['A', 'B', 'C', 'D'])
+		]
+	)
+	def test_get_name_at_pos(
+			self, file_path: str, line: int, char: int, expected: Tuple[str, int, int],
+		):
+		"""Unit test for get_name_at_pos()."""
+		with open(file_path) as f:
+			lines = f.readlines()
+			assert MROCalculator.get_name_at_pos([""] + lines, line, char) == expected
